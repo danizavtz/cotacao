4fcebdb5f39a950a7611e7398ae8f1300ef6b981
danizavtz <daniellucena@yahoo.com.br>
2023-09-15 09:38:05 -0300

    Removing morgan as logger

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 require('dotenv').config()
 const express = require('express');
-const logger = require('morgan');
 const cors = require('cors');
 
 const app = express();
@@ -8,10 +7,7 @@ cors({ credentials: true, origin: true });
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
- 
-if (process.env.NODE_ENV !== 'test') {
-  app.use(logger('dev'));
-}
+
 app.use(require('./server/index'));
 
 module.exports = app;
